926182403d03caa3b92e7c16c1646ad2336779b2
Piyush Mohapatra <piyush.ofcl@gmail.com>
2023-05-07 18:52:00 +0530

    Update AddRoom.jsx

--- client/src/components/AddRoom.jsx
+++ client/src/components/AddRoom.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef, useEffect } from 'react';
 import axios from "axios";
 
 function AddRoom(){
@@ -30,23 +30,46 @@ function AddRoom(){
             console.log(error);
         }
     };
+
+    const clearform = () => {
+        setRoomNumber("");
+        setType("");
+        setFloor("");
+        setBlockno("");
+    };
+
+    
     const submitHandler = function (e) {
         e.preventDefault();
         AddRoom();
+        clearform();
     };
+    useEffect(() => {
+        if (roomNumber.length === 3) {
+          setFloor(roomNumber.charAt(0));
+        } else if (roomNumber.length === 4) {
+          setFloor(roomNumber.substring(0, 2));
+        } else {
+          setFloor('');
+        }
+      }, [roomNumber]);
+    
+
+
+
 
     return (
         
         <div className="mx-auto w-full max-w-[550px]">
                 <form onSubmit={submitHandler} action="" method="POST">
                 <div className="mb-5">
-                <label
+                    <label
                     htmlFor="roomNumber"
                     className="mb-3 block text-base font-medium text-[#07074D]"
-                >
+                    >
                     Room Number
-                </label>
-                <input
+                    </label>
+                    <input
                     type="text"
                     ref={roomNumberEl}
                     name="roomNumber"
@@ -54,72 +77,84 @@ function AddRoom(){
                     value={roomNumber}
                     placeholder="Room Number"
                     onChange={() => {
-                    setRoomNumber(roomNumberEl.current.value);
-                    }}
+                        setRoomNumber(roomNumberEl.current.value);
+                        }}
+                    maxLength={4}
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                />
+                    />
                 </div>
+
+
                 <div className="mb-5">
-                <label
-                    htmlFor="type"
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                >
-                    Type
-                </label>
-                <input
-                    type="text"
-                    ref={typeEl}
-                    name="type"
-                    id="type"
-                    value={type}
-                    placeholder="Type"
-                    onChange={() => {
-                    setType(typeEl.current.value);
-                    }}
-                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                />
-                </div>
-                <div className="mb-5">
-                <label
-                    htmlFor="floor"
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                >
-                    Floor
-                </label>
-                <input
-                    type="text"
-                    name="floor"
-                    ref={floorEl}
-                    id="floor"
-                    value={floor}
-                    onChange={() => {
-                    setFloor(floorEl.current.value);
-                    }}
-                    placeholder="Floor"
-                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                />
-                </div>           
+                    <label
+                        htmlFor="type"
+                        className="mb-3 block text-base font-medium text-[#07074D]"
+                    >
+                        Type
+                    </label>
+                    <select
+                        id="type"
+                        name="type"
+                        value={type}
+                        onChange={(event) => {
+                        setType(event.target.value);
+                        }}
+                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    >
+                        <option value="">Select Room Type</option>
+                        <option value="1bhk">1 BHK</option>
+                        <option value="2bhk">2 BHK</option>
+                        <option value="3bhk">3 BHK</option>
+                    </select>
+                    </div>
+
+                    <div className="mb-5">
+                        <label
+                        htmlFor="floor"
+                        className="mb-3 block text-base font-medium text-[#07074D]"
+                        >
+                        Floor
+                        </label>
+                        <input
+                        type="text"
+                        name="floor"
+                        ref={floorEl}
+                        id="floor"
+                        value={floor}
+                        placeholder="Floor"
+                        onChange={() => {
+                            setFloor(floorEl.current.value);
+                        }}
+                        disabled={true}
+                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                        />
+                    </div>
                 
-                <div className="mb-5">
-                <label
-                    htmlFor="block-no"
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                >
-                    Block Number
-                </label>
-                <input
-                    type="text"
-                    ref={blocknoEl}
-                    name="block-no"
-                    id="block-no"
-                    value={blockno}
-                    placeholder="Block Number"
-                    onChange={() => {
-                    setBlockno(blocknoEl.current.value);
-                    }}
-                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                />
-                </div>
+                    <div className="mb-5">
+                    <label
+                        htmlFor="block-no"
+                        className="mb-3 block text-base font-medium text-[#07074D]"
+                    >
+                        Block Number
+                    </label>
+                    <select
+                        ref={blocknoEl}
+                        name="block-no"
+                        id="block-no"
+                        value={blockno}
+                        onChange={() => {
+                        setBlockno(blocknoEl.current.value);
+                        }}
+                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    >
+                        <option value="">Select Block Number</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                    </div>
+
                 <div className="flex w-full">
                 <button className="mx-auto hover:shadow-form  py-3 px-8 text-white bg-blue-500 rounded-md focus:bg-blue-600 focus:outline-none hover:bg-white hover:text-blue-500 transition-all duration-300 hover:border-blue-500 border-transparent border-2">
                     Submit
